db4b1a0aee3283712f4c4726f7a853e617155da6
Gattaka20044 <nexti20044@mail.ru>
2022-05-18 22:30:29 +0300

    Глава 3. Жизненный цикл Activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,13 +2,18 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+
+
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -29,6 +34,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG,"onCreate(Bundle?) called")
+
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -67,5 +75,30 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
